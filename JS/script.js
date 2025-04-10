fbdbb5267f14f953cb3b0c1106f41ccf9f349efc
Ayomide Jeremiah <Ayomide Jeremiah>
2025-04-10 11:19:20 +0100

    Re-edited JavaSCRIPT File

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,37 @@
-(document).ready(function () {
+
+$(document).ready(function () {
   $('#admissionForm').on('submit', function (e) {
     e.preventDefault();
-    $('#formMessages').empty();
 
     const name = $('#name').val().trim();
     const age = $('#age').val();
     const dob = $('#dob').val();
     const course = $('#course').val();
 
-    name.length === 0 && $('#formMessages').append('<p class="error">Name is required.</p>');
-    !age && $('#formMessages').append('<p class="error">Age is required.</p>');
-    !dob && $('#formMessages').append('<p class="error">Date of Birth is required.</p>');
-    course.length === 0 && $('#formMessages').append('<p class="error">Please select a course.</p>');
+    $('.name').text(name)
+    $('.age').text(age)
+    $('.date').text(dob)
+    $('.course').text(course)
 
-    name && age && dob && course && $('#formMessages').append(`
-      <div class="success">
-        <h4>MCROYAL SCHOOL - Admission Confirmation</h4>
-        <p>Dear <strong>${name} </strong>,</p>
-        <p>Thank you for applying to <strong>Acedu College</strong>. We have successfully received your application to study <strong>${course}</strong>.</p>
-        <p>Our admissions department will carefully review your details. If you meet the requirements, you will be contacted for the next stage of the process.</p>
-        <p>We appreciate your interest in becoming a part of the MCROYAL community and wish you the best of luck!</p>
-      </div>
-    `);    
+    $('#name').val('')
+    $('#age').val('')
+    $('#dob').val('')
+    $('#course').val('')
+
+    name.length === 0 && $('.messerror').html('<p class="error">Name is required.</p>');
+    age < 14 && $('.messerror').html(`<p class="error">Age is required and it can't be less than 14 .</p>`);
+    !dob && $('.messerror').html('<p class="error">Date of Birth is required.</p>');
+    course.length === 0 && $('.messerror').html('<p class="error">Please select a course.</p>');
+
+    name.length > 0 && age > 13 && dob && course.length > 0
+     && $('.success').slideDown() 
+     && $('#admissionForm').slideUp() 
+     && $('.messerror').empty() 
+ 
+  });
+
+  $('.edit').on('click', function () {
+    $('.success').slideUp() 
+     $('#admissionForm').slideDown() 
   });
 });
-$
